2bb5067cd073e816ed6c4a2ada2d931c8194e4fe
Timo Kirkkala <timo.kirkkala@gmail.com>
2010-11-22 18:26:30 +0000

    The new kalak.org flickr thing commit
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@2 268cc9b1-ce9d-41c3-bf31-a735b2610903

---
+++ web/assets/analytics.php
@@ -0,0 +1,17 @@
+<?php
+	// Include once these cool tracking scripts
+?>
+
+<script type="text/javascript"> 
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script> 
+<script type="text/javascript"> 
+try {
+var pageTracker = _gat._getTracker("UA-2711157-1");
+pageTracker._trackPageview();
+} catch(err) {}</script> 
+ 
+<!-- BEGIN Snoobi v1.4 --> 
+<script type="text/javascript" src="http://eu1.snoobi.com/snoop.php?tili=kalak_org"></script> 
+<!-- END Snoobi v1.4 --> 
