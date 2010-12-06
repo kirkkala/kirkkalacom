cb8b39d4f1fa71293f65575a7f51d75146db5f2b
Timo Kirkkala <timo.kirkkala@gmail.com>
2010-12-06 12:19:47 +0000

    404 - the page.
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@22 268cc9b1-ce9d-41c3-bf31-a735b2610903

---
+++ web/404.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fi" lang="fi">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+<meta name="keywords" content="Timo Kirkkala, valokuvaaja, valokuvia, photos, photoblog, flickr" />
+<meta name="description" content="Photos by mr Timo Kirkkala - the flickr API experiment." />
+<meta name="copyright" content="Timo Kirkkala" />
+<title>404 - page not found | Timo Kirkkala | Kalak.org</title>
+
+<link rel="stylesheet" href="/assets/css/style.css" type="text/css" />
+<script type="text/javascript" src="/assets/js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript" src="/assets/js/script.js"></script>
+
+</head>
+<body class="nojs">
+<div id="wrapper">
+<div id="header">
+<h1><a href="/"><span>This is </span> kalak.org <span> - the internetplayground of mr. Timo Kirkkala</span></a></h1>
+
+
+<div class="clear"></div>
+
+</div><!-- /header -->
+
+<div id="content">
+
+<div id="collaguae">
+<div id="info">
+<div id="photoset_img"><a href="/" title="Back to frontpage"><img src="http://farm4.static.flickr.com/3093/buddyicons/29955877@N04.jpg" alt="Kalak" /></a></div>
+
+<div id="photoset_desc">
+<h2>Page not found. Buu.</h2>
+<p>Unfortunately, the page you were looking for does not exist.<br />
+If you really miss the content you were looking for, drop me an email to <a href="mailto:timo.kirkkala@gmail.com">timo.kirkkala@gmail.com</a> and I might revert that to the site.</p>
+<p><a href="http://www.kalak.org/">www.kalak.org</a></p>
+</div>
+
+</div><!-- /info -->
+
+
+</a><div id="copy">
+<p>&copy; Timo Kirkkala 2010</p>
+</div><!-- /copy -->
+<div class="clear"></div>
+
+</div><!-- /collaguae -->
+
+</div><!-- /content -->
+</div><!-- /wrapper -->
+
+
+<script type="text/javascript"> 
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script> 
+<script type="text/javascript"> 
+try {
+var pageTracker = _gat._getTracker("UA-2711157-1");
+pageTracker._trackPageview();
+} catch(err) {}</script> 
+
+<!-- BEGIN Snoobi v1.4 --> 
+<script type="text/javascript" src="http://eu1.snoobi.com/snoop.php?tili=kalak_org"></script> 
+<!-- END Snoobi v1.4 --> 
+
+</body>
+</html>
