b594ce15ee68763be1af46d923a1bbafd51376a7
Timo Kirkkala <timo.kirkkala@gmail.com>
2010-12-06 13:08:08 +0000

    deny viewing the functions.php
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@27 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/index.php
+++ web/index.php
@@ -1,4 +1,7 @@
-<?php require_once('assets/functions.php'); ?>
+<?php 
+define("AUTH",1);  
+require_once('assets/functions.php'); 
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fi" lang="fi">
 <head>
