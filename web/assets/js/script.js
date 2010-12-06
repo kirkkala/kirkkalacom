f4e9411b86b062ae1e3eac983e27cd85ff4c26b4
Timo Kirkkala <timo.kirkkala@gmail.com>
2010-12-06 10:00:37 +0000

    changes before the 1st 'prod release'
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@6 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
   // Change the list to cool dropdown
   // Remember to enable  & include the-js
-//  $.simpledropdown("#select_form");
+  $.simpledropdown("#select_form");
     
   // Test for dummy browsers
   if(jQuery.support.opacity) {
@@ -16,19 +16,8 @@ $(document).ready(function() {
       $("body").html("<img src=\"http://dev.kalak/phpflickr/img/loading.gif\" />");
     });
     $("body").fadeIn("20000");
-    
-    //$('#collaguae a').shuffle(); 
   }
- 
-  // http://flowplayer.org/tools/tooltip/
-  $("#collaguae img[title]").tooltip({
-    effect: 'slide',
-    direction: 'up',
-    slideOffset: 10,
-    bounce: true,
-    delay: 0
-  });
-  
+
   $("a[rel=group]").fancybox({
     'overlayColor'    : '#000',
 		'overlayOpacity'  : 0.7,
@@ -38,7 +27,7 @@ $(document).ready(function() {
     'speedOut'        : 600,
     'titlePosition'   : 'outside',
     'titleFormat'     : function(title, currentArray, currentIndex, currentOpts) {
-      return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+      return '<span id="fancybox-title-over"><span>' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? '</span> ' + title : '') + '</span>';
     }
 	});
 
