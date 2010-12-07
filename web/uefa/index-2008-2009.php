f92965a23f1832c016dbecd5c923913aa361e880
Timo Kirkkala <timo.kirkkala@gmail.com>
2010-12-07 18:25:01 +0000

    added uefa dir
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@36 268cc9b1-ce9d-41c3-bf31-a735b2610903

---
+++ web/uefa/index-2008-2009.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+"http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<title>UEFA - Beerbets</title>
+</head>
+<frameset rows="*">
+<frame frameborder="0" src="http://spreadsheets.google.com/pub?key=pTG8eD3i2XBSqRzGOAQk_Pw">
+<noframes>
+<body>
+
+</body>
+</noframes>
+</frameset>
+</html>
---
+++ web/uefa/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+"http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<title>UEFA Beerbets 2010-2011</title>
+</head>
+<frameset rows="*">
+<frame frameborder="0" src="https://spreadsheets.google.com/pub?key=0AnSiaCt8uU-pdE12TlN2NjUxS0l6RjBFMHdET2JfWmc">
+<noframes>
+<body>
+
+</body>
+</noframes>
+</frameset>
+</html>
