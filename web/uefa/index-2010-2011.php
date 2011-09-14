d7672a2a8b6c3bd1b09d28da89fe81b96694bb33
Timo Kirkkala <timo.kirkkala@gmail.com>
2011-09-14 11:59:03 +0000

    New beerbet season, archived 2010-2011
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@46 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/uefa/index-2010-2011.php
+++ web/uefa/index-2010-2011.php
@@ -5,7 +5,7 @@
 <title>UEFA Beerbets 2010-2011</title>
 </head>
 <frameset rows="*">
-<frame frameborder="0" src="https://docs.google.com/spreadsheet/pub?hl=en_US&amp;hl=en_US&amp;key=0AsZvFsBzLnrJdFkxQlZtN3ZrSHNHS2JZSXlPOE1laVE&amp;output=html">
+<frame frameborder="0" src="https://spreadsheets.google.com/pub?key=0AnSiaCt8uU-pdE12TlN2NjUxS0l6RjBFMHdET2JfWmc">
 <noframes>
 <body>
 
