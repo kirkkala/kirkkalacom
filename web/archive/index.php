e58d6a2801900a9acad383f913ea9ff68c8e6f8b
Timo Kirkkala <timo.kirkkala@gmail.com>
2012-01-16 21:34:52 +0000

    Added archive index
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@57 268cc9b1-ce9d-41c3-bf31-a735b2610903

---
+++ web/archive/index.php
@@ -0,0 +1,36 @@
+
+<!doctype html>
+<!--[if lt IE 7]> <html lang="en-us" class="ie6"> <![endif]-->
+<!--[if IE 7]>    <html lang="en-us" class="ie7"> <![endif]-->
+<!--[if IE 8]>    <html lang="en-us" class="ie8"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en-us"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge;chrome=1" >
+  <meta name="description" content="Homepage snapshots of kalak.org" />
+  <meta name="copyright" content="Timo Kirkkala" />  
+	<title>Archives | Kalak.org</title>
+	
+</head>
+<body class="nojs">
+<h1>This is how kalak.org used to look like in...</h1>
+<ul>
+<li><a href="2010/">2010</a></li>
+<li><a href="2011/">2010-2012</a></li>
+</ul>
+
+<script type="text/javascript"> 
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script> 
+<script type="text/javascript"> 
+try {
+var pageTracker = _gat._getTracker("UA-2711157-1");
+pageTracker._trackPageview();
+} catch(err) {}</script> 
+ 
+<!-- BEGIN Snoobi v1.4 --> 
+<script type="text/javascript" src="http://eu1.snoobi.com/snoop.php?tili=kalak_org"></script> 
+<!-- END Snoobi v1.4 --> 
+</body>
+</html>
