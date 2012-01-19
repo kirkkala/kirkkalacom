39d2289d736fa2c8ec75cfb4c6ec3b4f617a1d93
Timo Kirkkala <timo.kirkkala@gmail.com>
2012-01-19 22:02:37 +0000

    Overlaybox added with functionality + other fancy crap
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@64 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,26 +1,19 @@
-/* Author: 
+/* Author: Timo Kirkkala
 
 */
+$(document).ready(function() {
+	$('#intro-container').hide().delay(1200).fadeIn(800);
+	$('.close').click(function() {
+		$('#intro-container').fadeOut(400);
+		$('#container .open').fadeIn(400);
+		$('.load-item').css('visibility', 'visible'); 
+	});
+	
+	$('.open').click(function() {
+		$('#intro-container').fadeIn(400);
+		$('#container .open').fadeOut(400);
+		$('.load-item').css('visibility', 'hidden'); 
+	});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
