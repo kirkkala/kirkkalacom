68f0b7229a5c0c4cc1e06fdd39b60b96066897f9
Timo Kirkkala <timo.kirkkala@gmail.com>
2012-01-21 11:56:30 +0000

    jQuery cookie and some further styling stuff
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@66 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -2,17 +2,27 @@
 
 */
 $(document).ready(function() {
-	$('#intro-container').hide().delay(1200).fadeIn(800);
+	var time = 400;
+	
+	if($.cookies.get('hasClosedIntro') !== true) {
+		$('#intro-container').hide().delay(1200).fadeIn(800);
+	}
+	else {
+		$('#container .open').fadeIn(time);
+		$('.load-item').hide().css('visibility', 'visible').fadeIn(time); 
+	}
+	
 	$('.close').click(function() {
-		$('#intro-container').fadeOut(400);
-		$('#container .open').fadeIn(400);
-		$('.load-item').css('visibility', 'visible'); 
+		$.cookies.set('hasClosedIntro', true);
+		$('#intro-container').fadeOut(time);
+		$('#container .open').fadeIn(time);
+		$('.load-item').hide().css('visibility', 'visible').fadeIn(time); 
 	});
 	
 	$('.open').click(function() {
-		$('#intro-container').fadeIn(400);
-		$('#container .open').fadeOut(400);
-		$('.load-item').css('visibility', 'hidden'); 
+		$('#intro-container').fadeIn(time);
+		$('#container .open').fadeOut(time);
+		$('.load-item').fadeOut(time); 
 	});
 });
 
