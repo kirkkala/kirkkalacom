f08cae51d0a5d250c88947243cc67488e022c78f
Timo Kirkkala <timo.kirkkala@gmail.com>
2012-01-25 16:17:45 +0000

    Querystring info opener
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@71 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,7 +1,25 @@
 /* Author: Timo Kirkkala
 	 jQuery Cookies docs: http://code.google.com/p/cookies/
 */
+
 $(document).ready(function() {
+
+	// Extend to get querystring for always displaying the info
+	$.extend({
+		getUrlVars: function(){
+			var vars = [], hash;
+			var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+			for(var i = 0; i < hashes.length; i++) {
+				hash = hashes[i].split('=');
+				vars.push(hash[0]);
+				vars[hash[0]] = hash[1];
+			}
+			return vars;
+		},
+		getUrlVar: function(name){
+			return $.getUrlVars()[name];
+		}
+	});
 
 	function blinkMe(element, delay) {
 		$(element).hide().delay(delay).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -9,7 +27,7 @@ $(document).ready(function() {
     
 	var time = 400;
 	
-	if($.cookies.get('hasClosedIntro') !== true) {
+	if($.cookies.get('hasClosedIntro') !== true || $.getUrlVar('info') === 'show') {
 		$('#info-container').hide().delay(20).fadeIn(time);
 		$('img').css('opacity', 0.05);
 		blinkMe('#closing-tip', 3000);
