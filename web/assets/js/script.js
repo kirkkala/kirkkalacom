1aeb5dfd76169c391749556e78ef0b9495cfed02
Timo Kirkkala <timo.kirkkala@gmail.com>
2015-09-22 20:09:52 +0000

    Do not show the infobox automatically. Trash teh jQuery cookie
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@110 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,41 +1,29 @@
-/* Author: Timo Kirkkala
-	 jQuery Cookies docs: http://code.google.com/p/cookies/
-*/
-
 $(document).ready(function() {
- 
+
 	var time = 400;
-		
-	if($.cookies.get('hide_info') !== true) {
-		$('#info-container').hide().delay(420).fadeIn(time);
-		$('img').css('opacity', 0.05);
-		blinkMe('#closing-tip', 3000);
-	}
-	else {
-		$('#container .open').fadeIn(time);
-		$('.load-item').hide().css('visibility', 'visible').fadeIn(time); 
-	}
-    	
+
+	$('#container .open').fadeIn(time);
+	$('.load-item').hide().css('visibility', 'visible').fadeIn(time);
+    
 	$('.close').click(function() {
-		$.cookies.set('hide_info', true, { expiresAt: new Date(2020,1,1) });
 		$('#info-container').fadeOut(time);
 		$('#container .open').fadeIn(time);
-		$('.load-item').hide().css('visibility', 'visible').fadeIn(time); 
+		$('.load-item').hide().css('visibility', 'visible').fadeIn(time);
 		$('img').css('opacity', 1);
 	});
-	
+
 	$('.open').click(function() {
 		$('#info-container').fadeIn(time);
 		$('#container .open').fadeOut(time);
-		$('.load-item').fadeOut(time); 
+		$('.load-item').fadeOut(time);
 		$('img').css('opacity', 0.05);
 		blinkMe('#closing-tip', 5000);
 	});
-	
+
 	function blinkMe(element, delay) {
 		$(element).hide().delay(delay).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 	}
-	
+
 });
 
 
