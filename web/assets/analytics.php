a1370bc25ec1ddcc4e5817198a6f0a76868076de
Timo Kirkkala <timo.kirkkala@gmail.com>
2015-09-22 19:56:56 +0000

    Commit all random changes
    git-svn-id: https://kalak.svn.beanstalkapp.com/kalak/trunk@109 268cc9b1-ce9d-41c3-bf31-a735b2610903

--- web/assets/analytics.php
+++ web/assets/analytics.php
@@ -2,22 +2,13 @@
 	// Include once these cool tracking scripts
 ?>
 
-<script type="text/javascript">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-2711157-1']);
-  _gaq.push(['_setDomainName', 'kirkkala.com']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  ga('create', 'UA-2711157-1', 'auto');
+  ga('send', 'pageview');
 
 </script>
- 
-<!-- BEGIN Snoobi v1.4 --> 
-<script type="text/javascript" src="http://eu1.snoobi.com/snoop.php?tili=kalak_org"></script> 
-<!-- END Snoobi v1.4 --> 
