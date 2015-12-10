2e8ff5930eb62f77467e10851b6de20f53337c38
Timo Kirkkala <timo.kirkkala@gmail.com>
2015-12-11 00:10:33 +0200

    Add a bunch of directories

---
+++ public/superveikkaus/2010/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+"http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<title>Superveikkaus 2010</title>
+</head>
+<frameset rows="*">
+<frame frameborder="0" src="https://spreadsheets.google.com/pub?key=0AsZvFsBzLnrJdHRFa0szRDMwOFBVTUVvc1F4ejJXTWc">
+<noframes>
+<body>
+
+</body>
+</noframes>
+</frameset>
+</html>
